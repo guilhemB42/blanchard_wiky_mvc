138552651109d97f9460d06a7f0cf213e3e6be54
guilhèm blanchard <github@gblanchard.eu>
2024-01-11 11:21:31 +0100

    ajout des entities

--- Entities/Article.cs
+++ Entities/Article.cs
@@ -1,7 +1,17 @@
-﻿namespace Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
 {
     public class Article
     {
-
+        public int Id { get; set; }
+        public string Theme { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Auteur { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime DateModification { get; set; }
+        public string Contenu { get; set; }
+        public List<Commentaire> Comments { get; set; }
     }
 }
--- Entities/Commentaire.cs
+++ Entities/Commentaire.cs
@@ -1,6 +1,22 @@
-﻿namespace Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Entities
 {
-    internal class Commentaire
+    public class Commentaire
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)] 
+        public string Auteur { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime DateModification { get; set; }
+        [StringLength(100)]
+        public string Contenu { get; set; }
+        public int ArticleId { get; set; }
+        public Article Article { get; set; }
+
+
     }
 }
