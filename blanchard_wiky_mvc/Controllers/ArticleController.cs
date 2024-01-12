56d9fd6e2ac69f366d04606bd6b5159362ca7567
guilhèm blanchard <github@gblanchard.eu>
2024-01-12 10:44:21 +0100

    ajout vue partielle articles

--- blanchard_wiky_mvc/Controllers/ArticleController.cs
+++ blanchard_wiky_mvc/Controllers/ArticleController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IRepositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace blanchard_wiky_mvc.Controllers
 {
     public class ArticleController : Controller
     {
-        public IActionResult Index()
+        IArticleRepository articleRepository;
+        public ArticleController(IArticleRepository IarticleRepository) { 
+            articleRepository = IarticleRepository;
+        }
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await articleRepository.GetAllAsync());
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllJson() { 
+            return PartialView("_displayArticles", await articleRepository.GetAllAsync());
         }
     }
 }
