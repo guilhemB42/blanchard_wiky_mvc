c19cc90a430baddef31daa348735cfb728624b69
guilhèm blanchard <github@gblanchard.eu>
2024-01-12 17:30:11 +0100

    ajout controleur commentaire

--- blanchard_wiky_mvc/Controllers/CommentaireController.cs
+++ blanchard_wiky_mvc/Controllers/CommentaireController.cs
@@ -1,12 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business;
+using Entities;
+using IBusiness;
+using Microsoft.AspNetCore.Mvc;
 
 namespace blanchard_wiky_mvc.Controllers
 {
     public class CommentaireController : Controller
     {
-        public IActionResult Index()
+        ICommentaireBusiness commentaireBusiness;
+        public CommentaireController(ICommentaireBusiness IcommentaireBusiness) {
+            commentaireBusiness = IcommentaireBusiness;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int Id)
+        {
+            return View(await commentaireBusiness.GetByIdAsync(Id));
+        }
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Commentaire commentaire)
+        {
+            if (!ModelState.IsValid )
+            {
+                return View(commentaire);
+            }
+            else
+            {
+                commentaire = await commentaireBusiness.CreateAsync(commentaire);
+                return RedirectToAction("Detail", new { Id = commentaire.Id });
+            }
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            Commentaire commentaire = await commentaireBusiness.GetByIdAsync(id);
+            return View(commentaire);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Commentaire commentaire)
+        {
+            commentaire = await commentaireBusiness.UpdateAsync(commentaire);
+            return RedirectToAction("Detail", new { Id = commentaire.Id });
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            await commentaireBusiness.DeleteByIdAsync(id);
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
