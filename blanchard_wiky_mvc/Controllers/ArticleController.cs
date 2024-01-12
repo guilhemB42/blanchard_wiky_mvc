2a0cc7638150a170de4959009292b62850017a98
guilhèm blanchard <github@gblanchard.eu>
2024-01-12 09:56:53 +0100

    ajout des views article et recherche, et les controleur article recherche et commentaire

---
+++ blanchard_wiky_mvc/Controllers/ArticleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace blanchard_wiky_mvc.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ blanchard_wiky_mvc/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace blanchard_wiky_mvc.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
