e3869fbacdbee4404040d715a8abf3f08fcb2eb6
guilhèm blanchard <github@gblanchard.eu>
2024-01-14 12:15:58 +0100

    début recherche

--- blanchard_wiky_mvc/Controllers/SearchController.cs
+++ blanchard_wiky_mvc/Controllers/SearchController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IBusiness;
+using Microsoft.AspNetCore.Mvc;
 
 namespace blanchard_wiky_mvc.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult Index()
+        IArticleBusiness articleBusiness;
+
+        public SearchController(IArticleBusiness IarticleBusiness) { 
+            articleBusiness = IarticleBusiness;
+        }
+        public async Task<IActionResult> Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Resultat() {
+            return View(await articleBusiness.GetAllAsync());
+        }
+
     }
 }
